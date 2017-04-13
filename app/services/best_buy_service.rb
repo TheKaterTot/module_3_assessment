f1510c4db6df1ec9a9d63098388c75b3f0120547
Katy Farmer <dreadpiratekaty@gmail.com>
2017-04-13 11:46:38 -0600

    Refactor parsing

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,11 +1,8 @@
 class BestBuyService
-  def initialize
-  end
 
   def self.get_stores_by_zip(zip)
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},10))?format=json&show=storeId,storeType,name,city,distance,phone&apiKey=#{ENV["KEY"]}")
-    body = JSON.parse(response.body)
-    body["stores"].map do |store_data|
+    JSON.parse(response.body)["stores"].map do |store_data|
       Store.new(store_data)
     end
   end
