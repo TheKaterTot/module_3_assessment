94c212b7992049340288d37e5d8d1c18678fb0cc
Katy Farmer <dreadpiratekaty@gmail.com>
2017-04-13 11:56:03 -0600

    Broke request with refactor

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,9 +1,13 @@
 class BestBuyService
-
   def self.get_stores_by_zip(zip)
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeId,storeType,name,city,distance,phone&apiKey=#{ENV["KEY"]}")
+    response = client("/stores(area(#{zip},25))?format=json&show=storeId,storeType,name,city,distance,phone&apiKey=#{ENV["KEY"]}")
     JSON.parse(response.body)["stores"].map do |store_data|
       Store.new(store_data)
     end
+  end
+
+  private
+  def client(path)
+    Faraday.get("https://api.bestbuy.com/v1" + path)
   end
 end
