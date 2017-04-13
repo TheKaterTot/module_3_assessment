d897784c48f4dbfa6a17ccdcc79126bb60131263
Katy Farmer <dreadpiratekaty@gmail.com>
2017-04-13 11:09:34 -0600

    Make zipcode search dynamic

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -3,7 +3,7 @@ class BestBuyService
   end
 
   def self.get_stores_by_zip(zip)
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80204,10))?format=json&show=storeId,storeType,name,city,distance,phone&apiKey=#{ENV["KEY"]}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},10))?format=json&show=storeId,storeType,name,city,distance,phone&apiKey=#{ENV["KEY"]}")
     body = JSON.parse(response.body)
     body["stores"].map do |store_data|
       Store.new(store_data)
