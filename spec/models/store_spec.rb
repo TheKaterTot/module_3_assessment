63d89a0caa85c7a7383c851030f3d11e47aa87c2
Katy Farmer <dreadpiratekaty@gmail.com>
2017-04-14 20:05:04 -0600

    Add all tests to store

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -5,11 +5,35 @@ describe Store do
                            "city" => "williams",
                            "distance" => 3.0,
                            "phone" => "1234567890",
-                           "store_type" => "box"}
+                           "storeType" => "box"}
                )}
   describe "#name" do
     it "returns the store name" do
       expect(store.name).to eq("hello")
+    end
+  end
+
+  describe "#phone" do
+    it "returns the store phone" do
+      expect(store.phone).to eq("1234567890")
+    end
+  end
+
+  describe "#city" do
+    it "returns the store city" do
+      expect(store.city).to eq("williams")
+    end
+  end
+
+  describe "#distance" do
+    it "returns the distance from the zip" do
+      expect(store.distance).to eq(3.0)
+    end
+  end
+
+  describe "#store_type" do
+    it "returns the type of store" do
+      expect(store.store_type).to eq("box")
     end
   end
 end
